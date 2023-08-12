099f4ea24285999612ce13e127a31510f0fecbcf
Daniel Bastos <danielsalles@gmail.com>
2023-08-12 18:15:26 -0300

    feat(chat): Added tables to chat dto

--- functions/shared/dtos/supabase/databaseChat.dto.ts
+++ functions/shared/dtos/supabase/databaseChat.dto.ts
@@ -3,6 +3,7 @@ export interface DatabaseChatDTO {
   title?: string;
   schema_id: string;
   user_id?: string;
+  tables: string[];
   created_at?: Date;
   updated_at?: Date;
   deleted_at?: Date | null;
